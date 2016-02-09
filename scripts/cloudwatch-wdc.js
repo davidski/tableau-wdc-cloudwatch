c0b5b928cd93d0bfaf2aeecbb0221d6dd5ff78f7
davidski <davidski@deadheaven.com>
2016-02-09 11:59:51 -0800

    Remove dependency on jQuery. Pull WDC SDK local.

--- scripts/cloudwatch-wdc.js
+++ scripts/cloudwatch-wdc.js
@@ -1,60 +1,12 @@
-(function() {
+function cwWdcInitialize() {
   var myConnector = tableau.makeConnector();
-  
+
   myConnector.init = function(){
     tableau.incrementalExtractColumn = "Timestamp";
     tableau.initCallback();
   };
 
-  function queryCloudWatch(username, secret, region, starttime, endtime, 
-                           metricname, namespace, dimensionname, 
-                           dimensionvalue, unit, period, statistics) {
-    /* the main CloudWatch worker function */
-    AWS.config.update({accessKeyId: username, secretAccessKey: secret});
-    AWS.config.region = region;
-    var cloudwatch = new AWS.CloudWatch();
-
-    var params = {
-      StartTime: starttime,
-      EndTime: endtime,
-      Namespace: namespace,
-      MetricName: metricname,
-      Period: period,
-      Statistics: statistics,
-      Dimensions: [{
-        Name: dimensionname,
-        Value: dimensionvalue
-      }
-    ],
-      Unit: unit
-    };
-    var request = cloudwatch.getMetricStatistics(params)
-
-    /* Invoke the call and take action based on the type of response */
-    try {
-      request.
-      on('success', function(data){
-        /* iterate over all the returned data points */
-        var lastRecordToken = 0;
-        tableau.dataCallback(data['data']['Datapoints'], lastRecordToken.toString(), true);
-      }).
-      on('error', function(err, data) {
-        tableau.abortWithError("Connection error " + err + "\nStack is: " + err.stack); // an error occurred
-      }).
-      on('complete', function(data) {
-        /* no more data, so return that fact to the WDC */
-        var lastRecordToken = 0;
-        tableau.dataCallback([], lastRecordToken.toString(), false);
-      }).
-      send();
-    }
-    catch(err) {
-      tableau.abortWithError("Connection error " + err + "\nStack is: " + err.stack);
-    }
-  }
-
   myConnector.getColumnHeaders = function() {
-    // support for querying multiple stats at once is not well-tested
     config = JSON.parse(tableau.connectionData);
     var fieldNames = ['Timestamp'];
     var fieldTypes = ['datetime'];
@@ -69,9 +21,10 @@
   }
 
   myConnector.getTableData = function(lastRecordToken) {
-    /* pull our config variables out of JSON and from Tableau WDC */
+    // pull our config variables out of JSON and from Tableau WDC
     config = JSON.parse(tableau.connectionData)
     myResponse = queryCloudWatch(
+      lastRecordToken,
       tableau.username, 
       tableau.password, 
       config['region'],
@@ -87,53 +40,121 @@
   }
 
   tableau.registerConnector(myConnector);
-})();
+  console.log("Connector now registered.");
 
-  $(document).ready(function() {
-    $("#submitButton").click(function() {
-      /* on submit, build our config settings from the form */
-      var akid  = $('#akid').val().trim();
-      var secret = $('#secret').val().trim();
-      var region = $('#region option:selected').val()
-      var metricname = $('#metricname').val().trim();
-      var namespace = $('#namespace').val().trim();
-      var dimensionname = $('#dimensionname').val().trim();
-      var dimensionvalue = $('#dimensionvalue').val().trim();
-      var starttime = new Date($('#starttime').val().trim());
-      var endtime = new Date($('#endtime').val().trim());
-      var unit = $('#unit').val().trim();
-      var period = parseInt($('#period').val().trim()) * 60;
-      var statistics = [];
-      $("#statistics:checked").each(function(){
-        statistics.push($(this).val());
-      });
-      if (metricname) {
-        /* so long as one metricname was submitted, try to query AWS */
-        tableau.username = akid;
-        tableau.password = secret;
-        tableau.connectionName = "AWS Data for " + metricname;
-        tableau.connectionData = JSON.stringify({
-          'region': region,
-          'metricname': metricname,
-          'namespace': namespace,
-          'dimensionname': dimensionname,
-          'dimensionvalue': dimensionvalue,
-          'starttime': starttime,
-          'endtime': endtime,
-          'period': period,
-          'unit': unit,
-          'statistics': statistics
-        });
-        tableau.submit();
-      }
-  });
-});
+};
+
+
+function queryCloudWatch(lastRecordToken, username, secret, region, 
+                         starttime, endtime, 
+                         metricname, namespace, dimensionname, 
+                         dimensionvalue, unit, period, statistics) {
+  /* the main CloudWatch worker function */
+  AWS.config.update({accessKeyId: username, secretAccessKey: secret});
+  AWS.config.region = region;
+  var cloudwatch = new AWS.CloudWatch();
+  var hasMoreData = false;
+
+  var params = {
+    StartTime: starttime,
+    EndTime: endtime,
+    Namespace: namespace,
+    MetricName: metricname,
+    Period: period,
+    Statistics: statistics,
+    Dimensions: [{
+      Name: dimensionname,
+      Value: dimensionvalue
+    }
+  ],
+    Unit: unit
+  };
+  var request = cloudwatch.getMetricStatistics(params)
+
+  /* Invoke the call and take action based on the type of response */
+  request.
+  on('success', function(data){
+    // dataCallback must be part of the success callback, since
+    // this operation is async
+    tableau.dataCallback(data['data']['Datapoints'], 
+                         lastRecordToken.toString(), hasMoreData);
+  }).
+  on('error', function(err, data) {
+    tableau.abortWithError("Connection error " + err + "\nStack is: " + 
+                           err.stack); // an error occurred
+  }).
+  send();
+
+};
 
 function printValue(sliderID, textbox) {
   /* helper function to display value of slider */
   var x = document.getElementById(textbox);
   var y = document.getElementById(sliderID);
   x.value = y.value;
-}
+};
+
+// pure JS version of jQuery documentready function
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+};
+
+ready(function(){
+  //prepare the Tableau WDC functions
+  cwWdcInitialize();
+  
+  //assign the submit event function
+  var element = document.getElementById("submitButton");
+  element.addEventListener("click", function(){
+    console.log("Button 'click' event detected.");
+    /* on submit, build our config settings from the form */
+    var akid  = document.querySelector("#akid").value.trim();
+    var secret = document.querySelector('#secret').value.trim();
+    var e = document.querySelector("#region")
+    var region = e[e.selectedIndex].value;
+    var metricname = document.querySelector('#metricname').value.trim();
+    var namespace = document.querySelector('#namespace').value.trim();
+    var dimensionname = document.querySelector('#dimensionname').value.trim();
+    var dimensionvalue = document.querySelector('#dimensionvalue').value.trim();
+    var starttime = new Date(document.querySelector('#starttime').value.trim());
+    var endtime = new Date(document.querySelector('#endtime').value.trim());
+    var unit = document.querySelector('#unit').value.trim();
+    var period = parseInt(document.querySelector('#period').value.trim()) * 60;
+    var statistics = [];
+    var checkboxes = document.querySelectorAll("#statistics");
+    Array.prototype.forEach.call(checkboxes, function(el, i){
+      if (el.checked = true) {
+        statistics.push(el.value);
+      };
+    });
+    if (metricname) {
+      /* so long as one metricname was submitted, try to query AWS */
+      tableau.username = akid;
+      tableau.password = secret;
+      tableau.connectionName = "AWS Data for " + metricname;
+      tableau.connectionData = JSON.stringify({
+        'region': region,
+        'metricname': metricname,
+        'namespace': namespace,
+        'dimensionname': dimensionname,
+        'dimensionvalue': dimensionvalue,
+        'starttime': starttime,
+        'endtime': endtime,
+        'period': period,
+        'unit': unit,
+        'statistics': statistics
+      });
+      console.log("About to tableau.submit()");
+      //tableau.initCallback();
+      tableau.submit();
+    }
+    console.log("Submit complete.");
+  });
+  
+});
 
 window.onload = function() { printValue('period', 'periodValue');  }
